34c3a3669e71a92319971a99133157af23f0155c
yangjun <yangjun1@meizu.com>
2021-01-29 18:23:11 +0800

    11

--- src/components/topNav/index.tsx
+++ src/components/topNav/index.tsx
@@ -7,19 +7,26 @@ const topNav = () =>{
         <div className="nav-box">
             <nav>
                 <div className="logo">
-                    <img src="" alt=""/>
+                    <img  src={require('./img/logo.png')}  alt=""/>
                 </div>
                 <div className="nav-right">
                     <ul className="nav-meun">
                         <li>
-                            <i className="nav-icon"></i>
-                            <span className="nav-name"></span>
+                            <i className="icon-game"></i>
+                            <span className="nav-name">游戏</span>
+                        </li>
+                        <li>
+                            <i className="icon-app"></i>
+                            <span className="nav-name">应用</span>
+                        </li>
+                        <li>
+                            <i className="icon-new"></i>
+                            <span className="nav-name">最新</span>
                         </li>
                     </ul>
                 </div>
             </nav>
         </div>
-        
     )
 }
 
